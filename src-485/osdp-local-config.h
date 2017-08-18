f478e2e607455f3287dc3cd5bcb2d26b860cdbe4
Jixun Wu <jixunw@bsquare.com>
2017-08-18 13:21:14 +0100

    Remove root permission required for socket.

--- src-485/osdp-local-config.h
+++ src-485/osdp-local-config.h
@@ -19,16 +19,29 @@
   http://www.securityindustry.org
 */
 
+#pragma once
+
+#include "open-osdp.h"
+
 // the server key and cert for the OSDP TLS server
 
 #define OSDP_LCL_CA_KEYS "/opt/osdp-conformance/etc/ca_keys.pem"
 #define OSDP_LCL_SERVER_CERT "/opt/osdp-conformance/etc/cert.pem"
 #define OSDP_LCL_SERVER_KEY "/opt/osdp-conformance/etc/key.pem"
+
+
+#if ENABLE_WEB_RPC
 #define OSDP_LCL_COMMAND_PATH "/opt/osdp-conformance/run/%s/open_osdp_command.json"
 #define OSDP_LCL_SERVER_RESULTS "/opt/osdp-conformance/run/%s"
 #define OSDP_LCL_UNIX_SOCKET "/opt/osdp-conformance/run/%s/open-osdp-control"
+#define OSDP_BASE_DIR "/opt/osdp-conformance/"
+#else
+#define OSDP_LCL_COMMAND_PATH "./tmp/command_%s.json"
+#define OSDP_LCL_SERVER_RESULTS "./tmp/result_%s"
+#define OSDP_LCL_UNIX_SOCKET "./tmp/socket_%s"
+#define OSDP_BASE_DIR "./tmp/"
+#endif
 
 #define OSDP_LCL_DEFAULT_PSK "speakFriend&3ntr"
 
-#define OSPD_LCL_SET_PID_TEMPLATE \
-    "sudo -n /opt/osdp-conformance/bin/set-pid %s %d"
+#define OSPD_LCL_SET_PID_TEMPLATE "/opt/osdp-conformance/bin/set-pid %s %d"
